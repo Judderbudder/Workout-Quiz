a09a5813f577cda1fc9147a48bc41b80edb7e416
Judson Dunne <41300381+Judderbudder@users.noreply.github.com>
2023-09-18 22:41:28 -0400

    Your commit message here

--- src/email/email.jsx
+++ src/email/email.jsx
@@ -30,7 +30,7 @@ const EmailQuestion = ({ question, onNext }) => {
           Name:
           <input 
             type="text" 
-            value={email} 
+            value={name} 
             onChange={(e) => setName(e.target.value)} 
             placeholder="John Doe"
           />
